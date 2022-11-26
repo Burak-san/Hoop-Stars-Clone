a0dd1ce28bb0c98634784dd7e73e88f3b31cdc54
Burak <60696929+Burak-san@users.noreply.github.com>
2022-11-26 17:56:02 +0300

    Player and Enemy movement added

---
+++ Assets/Scripts/Controllers/Enemy/EnemyMovementController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Controllers.Enemy
+{
+    public class EnemyMovementController : MonoBehaviour
+    {
+        private Rigidbody _enemyRigidbody;
+
+        private void Awake()
+        {
+            Init();
+        }
+        
+        private void Init()
+        {
+            _enemyRigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            EnemyMove();
+        }
+
+        private void EnemyMove()
+        {
+            if (_enemyRigidbody.position.x > 0 && _enemyRigidbody.position.y < 10)
+            {
+                EnemyLeftMove();
+            }
+            else if (_enemyRigidbody.position.x < 0 && _enemyRigidbody.position.y < 10)
+            {
+                EnemyRightMove();
+            }
+        }
+
+        private void EnemyLeftMove()
+        {
+            _enemyRigidbody.velocity = new Vector3(-2.5f,12,0);
+        }
+
+        private void EnemyRightMove()
+        {
+            _enemyRigidbody.velocity = new Vector3(2.5f,12,0);
+        }
+    }
+}
---
+++ Assets/Scripts/Controllers/Player/PlayerMovementController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Enums;
+using UnityEngine;
+
+namespace Controllers.Player
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        public void Move(PlayerState playerState)
+        {
+            if (playerState == PlayerState.Left)
+            {
+                LeftButtonMove();
+            }
+            else if (playerState == PlayerState.Right)
+            {
+                RightButtonMove();
+            }
+        }
+
+        private void LeftButtonMove()
+        {
+            //_rigidbody.AddForce(new Vector3(-2.5f,5,0),ForceMode.Impulse);
+            _rigidbody.velocity = new Vector3(-2.5f, 5, 0);
+            CheckRigidbodyKinematic();
+        }
+        
+        private void RightButtonMove()
+        {
+            //_rigidbody.AddForce(new Vector3(2.5f,5,0),ForceMode.Impulse);
+            _rigidbody.velocity = new Vector3(2.5f, 5, 0);
+            CheckRigidbodyKinematic();
+        }
+
+        private void CheckRigidbodyKinematic()
+        {
+            if (_rigidbody.isKinematic == true)
+            {
+                _rigidbody.isKinematic = false;
+            }
+        }
+    }
+}
